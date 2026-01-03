872828b7b587da502194e9f06eac2e9bbf203dbf
astrayl3 <nbd112nbd112@gmail.com>
2026-01-03 17:12:41 +0700

    forum app

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <h1>
+          <a href="/">Our Wonderful App</a>
+        </h1>
+      </header>
+
+      <main>
+        <h3>Hello this is the homepage</h3>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias magnam dolorem amet exercitationem eos voluptates debitis facere vitae repellat. Est ipsa perferendis consectetur tempore quisquam tenetur, aliquid dolores laudantium voluptatibus.
+        </p>
+      </main>
+
+      <footer>
+        <small>&copy; Our Wonderful App</small>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
